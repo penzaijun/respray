c0f9ffcffe4bc14f8501a3f0b6c44fbb27df0b80
falcon-sqh <1399792450@qq.com>
2018-11-24 18:57:02 +0800

    第一次提交代码

---
+++ Assets/Scripts/StarControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StarControl : MonoBehaviour {
+
+	public GameObject star;
+	private GameObject[] stars;
+	private Transform holder;
+	public RectTransform TransformRange;
+	// Use this for initialization
+	void Start () {
+		float w = TransformRange.sizeDelta.x/100;
+		float h = TransformRange.sizeDelta.y/100; 
+		Debug.Log("w="+w+"      h="+h);
+		for (int i=1;i<=10;i++){
+			GameObject instance =Instantiate(star, 
+				new Vector3(Random.Range(-w,w),Random.Range(-h,h),0f),
+				Quaternion.identity);
+			instance.transform.SetParent(TransformRange);
+			instance.transform.localScale=Vector3.one*1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
