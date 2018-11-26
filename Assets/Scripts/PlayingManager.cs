1e70b2d6320cfa1b89bee0587435bc32e89f9625
falcon-sqh <1399792450@qq.com>
2018-11-26 15:29:46 +0800

    11.26修改版

---
+++ Assets/Scripts/PlayingManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingManager : MonoBehaviour {
+	private List <Vector3> positions = new List <Vector3> ();
+	public List <GameObject> BlackHoles=new List <GameObject> ();
+	public GameObject blackhole;
+	public int number=1;
+	// Use this for initialization
+	void Start () {
+		for (int i=0;i<number;i++){
+			positions.Add(new Vector3(Random.Range(-4,4),Random.Range(-4,4),0f));
+		}
+		for (int i=0;i<number;i++){
+			GameObject instance = Instantiate(blackhole,positions[i],Quaternion.identity);
+			BlackHoles.Add(instance);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
