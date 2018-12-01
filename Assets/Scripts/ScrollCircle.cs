fb20118ea4a0b084c4ebdc1c33bc3139b8308597
woluo <fjjth98@163.com>
2018-12-01 11:12:33 +0800

    添加除按钮外目前所有元素的贴图

---
+++ Assets/Scripts/ScrollCircle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollCircle : ScrollRect
+{
+    // 玩家
+    public GameObject player;
+
+    // 半径
+    private float _mRadius = 0f;
+
+    // 距离
+    private const float Dis = 0.5f;
+
+    protected override void Start()
+    {
+        // 获取当前玩家
+        player = GameObject.Find("Player");
+
+        base.Start();
+
+        // 能移动的半径 = 摇杆的宽 * Dis
+        _mRadius = content.sizeDelta.x * Dis;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+
+        // 获取摇杆，根据锚点的位置。
+        Vector2 contentPosition = content.anchoredPosition;
+
+        // 判断摇杆的位置 是否大于 半径
+        if (contentPosition.magnitude > _mRadius)
+        {
+            // 设置摇杆最远的位置
+            contentPosition = contentPosition.normalized * _mRadius;
+            SetContentAnchoredPosition(contentPosition);
+        }
+
+        // 最后根据anchoredPosition确定施加的力
+        Vector2 v2 = content.anchoredPosition;
+        player.GetComponent<Rigidbody>().AddForce(v2);
+    }
+}
--- Assets/Scripts/TransferGate.cs
+++ Assets/Scripts/TransferGate.cs
@@ -11,8 +11,11 @@ public class TransferGate : MonoBehaviour {
     // 判断当前是否可以有效
     private bool isEff;
 
-	// Use this for initialization
-	void Start () {
+    // 旋转的组件，绕z轴旋转
+    private Vector3 ax = new Vector3(0.0f, 0.0f, 1.0f);
+
+    // Use this for initialization
+    void Start () {
         // 获取目标的坐标
         v3Target = tgTarget.transform.position;
         // 获取目标的半径
@@ -20,9 +23,10 @@ public class TransferGate : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+    // 实时旋转
 	void Update () {
-		
-	}
+        transform.Rotate(ax, 50 * Time.deltaTime, Space.Self);
+    }
 
     // 碰撞进入
     private void OnTriggerEnter(Collider other)
