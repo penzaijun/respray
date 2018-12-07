6263cca9444f9cecdbd6797970d50255bf876303
falcon-sqh <1399792450@qq.com>
2018-12-07 23:37:03 +0800

    未作大修改，仅稍微修改了assets架构

--- Assets/Scripts/PlayingManager.cs
+++ Assets/Scripts/PlayingManager.cs
@@ -6,6 +6,7 @@ public class PlayingManager : MonoBehaviour {
 	private List <Vector3> positions = new List <Vector3> ();
 	public List <GameObject> BlackHoles=new List <GameObject> ();
 	public GameObject blackhole;
+	public GameObject transferGate;
 	public int number=1;
 	// Use this for initialization
 	void Start () {
@@ -16,8 +17,10 @@ public class PlayingManager : MonoBehaviour {
 			GameObject instance = Instantiate(blackhole,positions[i],Quaternion.identity);
 			BlackHoles.Add(instance);
 		}
-	}
+		Vector3 pos=new Vector3(Random.Range(-4,4),Random.Range(-4,4),0f);
+		Instantiate(transferGate,pos,Quaternion.identity);
 	
+	}
 	// Update is called once per frame
 	void Update () {
 		
