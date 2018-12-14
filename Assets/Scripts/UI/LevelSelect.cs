9732ddea9c2d1ef08b6cb1de01d06e1edf533642
woluo <fjjth98@163.com>
2018-12-14 23:12:42 +0800

    change ui

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+
+    public int levelnum=0;
+    public Text leveltext;
+
+	// Use this for initialization
+	void Start () {
+        leveltext.text = levelnum.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
