88e070c8207d103e13635b0791196cf91c76cb81
penzai <penzaijun@gmail.com>
2018-12-14 19:30:39 +0800

    Signed-off-by: penzai <penzaijun@gmail.com>

---
+++ Assets/Scripts/UserData/Setting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Awake()
+    {
+        //process first open case
+        if(!PlayerPrefs.HasKey("firstopen"))
+        {
+            PlayerPrefs.SetInt("firstopen", 1);
+            PlayerPrefs.SetInt("music", 50);        // music strenth:[0,100]
+        }
+        //normal case
+        else
+        {
+            
+        }
+    }
+}
