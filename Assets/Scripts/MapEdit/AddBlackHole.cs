5f6ba3bf24a14a908af964d8e341ac2cd40d248d
lfh <a501943907@outlook.com>
2018-12-28 19:32:09 +0800

    增加增加save按钮，归类脚脚本

---
+++ Assets/Scripts/MapEdit/AddBlackHole.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddBlackHole : MonoBehaviour
+{
+    public GameObject blackHolePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickBlackHole()
+    {
+        GameObject blackHole = GameObject.Instantiate(blackHolePrefab, Vector3.zero, Quaternion.identity);
+        blackHole.AddComponent<MouseDrag>();
+        MapEditList.blackHoleList.Add(blackHole);
+    }
+}
