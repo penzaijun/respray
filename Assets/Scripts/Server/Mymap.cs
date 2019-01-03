8d8f906dfc65a683dd9fcef39675ada15e83869e
penzai <penzaijun@gmail.com>
2019-01-04 04:43:37 +0800

    fix script dirctory
    Signed-off-by: penzai <penzaijun@gmail.com>

--- Assets/Scripts/Server/Mymap.cs
+++ Assets/Scripts/Server/Mymap.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Myamap : MonoBehaviour {
 
-    // FileName
-    public string Filename;
+    public int i;
+
 	// Use this for initialization
 	void Start () {
 		
