8c07888046701c5b50d8443b3a1a361b85bd3296
yuweiming70 <32589058+yuweiming70@users.noreply.github.com>
2019-01-04 14:35:20 +0800

    读取到json已经可用

--- Assets/Scripts/PlayingManager.cs
+++ Assets/Scripts/PlayingManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,7 +46,7 @@ public class PlayingManager : MonoBehaviour {
     void Loadmap(string path)
     {
     //load the scene json from file
-    
+    print("load map!");
     StreamReader json = File.OpenText(path);
     string input = json.ReadToEnd();
     JsonData jd = JsonMapper.ToObject(input);
@@ -63,7 +63,7 @@ public class PlayingManager : MonoBehaviour {
         GameObject instance = Instantiate(hole, positions_black_hole, Quaternion.identity);
         instance.GetComponent<ForceField>().magn = abs(force);
         instance.GetComponent<ForceField>().isAttract = !sign(force);
-        instance.transform.localScale *= size;
+        instance.transform.localScale = new Vector3(size,size,1);
         BlackHoles.Add(instance);
     }
 
@@ -84,8 +84,8 @@ public class PlayingManager : MonoBehaviour {
             GameObject gate = (GameObject)Resources.Load("Prefabs/GameObject/TransferGate");
             GameObject gate_object_A = Instantiate(gate, Apos, Quaternion.identity);
             GameObject gate_object_B = Instantiate(gate, Bpos, Quaternion.identity);
-            gate_object_A.transform.localScale *= size;
-            gate_object_B.transform.localScale *= size;
+            gate_object_A.transform.localScale = new Vector3(size,size,1);
+            gate_object_B.transform.localScale = new Vector3(size,size,1);
             TransferGates.Add(gate_object_A);
             TransferGates.Add(gate_object_B);
             gate_object_A.GetComponent<TransferGate>().tgTarget = gate_object_B.GetComponent<TransferGate>();
@@ -110,9 +110,9 @@ public class PlayingManager : MonoBehaviour {
             GameObject gate_object_A = Instantiate(gate, Apos, Quaternion.identity);
             GameObject gate_object_B = Instantiate(gate, Bpos, Quaternion.identity);
             GameObject gate_object_C = Instantiate(gate, Cpos, Quaternion.identity);
-            gate_object_A.transform.localScale *= size;
-            gate_object_B.transform.localScale *= size;
-            gate_object_C.transform.localScale *= size;
+            gate_object_A.transform.localScale = new Vector3(size,size,1);
+            gate_object_B.transform.localScale = new Vector3(size,size,1);
+            gate_object_C.transform.localScale = new Vector3(size,size,1);
             TransferGates.Add(gate_object_A);
             TransferGates.Add(gate_object_B);
             TransferGates.Add(gate_object_C);
@@ -144,10 +144,10 @@ public class PlayingManager : MonoBehaviour {
             GameObject gate_object_B = Instantiate(gate, Bpos, Quaternion.identity);
             GameObject gate_object_C = Instantiate(gate, Cpos, Quaternion.identity);
             GameObject gate_object_D = Instantiate(gate, Dpos, Quaternion.identity);
-            gate_object_A.transform.localScale *= size;
-            gate_object_B.transform.localScale *= size;
-            gate_object_C.transform.localScale *= size;
-            gate_object_D.transform.localScale *= size;
+            gate_object_A.transform.localScale = new Vector3(size,size,1);
+            gate_object_B.transform.localScale = new Vector3(size,size,1);
+            gate_object_C.transform.localScale = new Vector3(size,size,1);
+            gate_object_D.transform.localScale = new Vector3(size,size,1);
             TransferGates.Add(gate_object_A);
             TransferGates.Add(gate_object_B);
             TransferGates.Add(gate_object_C);
@@ -170,9 +170,11 @@ public class PlayingManager : MonoBehaviour {
         float x = float.Parse(jd["star"][i]["x"].ToString());
         float y = float.Parse(jd["star"][i]["y"].ToString());
         float size = float.Parse(jd["star"][i]["size"].ToString());
+        float angle = float.Parse(jd["star"][i]["angle"].ToString());
         Vector3 pos = new Vector3(x, y, 0f);
         GameObject star_obj = Instantiate(star, pos, Quaternion.identity);
-        star_obj.transform.localScale *= size;
+        star_obj.transform.localEulerAngles = new Vector3(0.0f,0.0f,angle);
+        star_obj.transform.localScale = new Vector3(size,size,1);
         Stars.Add(star_obj);
     }
 
@@ -183,130 +185,161 @@ public class PlayingManager : MonoBehaviour {
         float y = float.Parse(jd["player"]["y"].ToString());
         float size = float.Parse(jd["player"]["size"].ToString());
         Vector3 pos = new Vector3(x, y, 0f);
-        //Player = Instantiate(player,pos,Quaternion.identity);
-        Player = GameObject.Find("Player");
+        Player = Instantiate(player,pos,Quaternion.identity);
+        //Player = GameObject.Find("Player");
         Player.transform.position = pos;
         Player_Defaultpos = pos;
-        Player.transform.localScale *= size;
+        Player.transform.localScale = new Vector3(size,size,1);
         Player_Defaultsize = size;
     }
 
-    //load end gate,must copy end gate
-    for (int i = 0; i < jd["Flag"].Count; i++)
-    {
-        //GameObject flag = GameObject.FindWithTag("Flag");
-        /*GameObject flag = (GameObject)Resources.Load("Prefabs/GameObject/Flag");
-        float x = float.Parse(jd["Flag"][i]["x"].ToString());
-        float y = float.Parse(jd["Flag"][i]["y"].ToString());
-        float size = float.Parse(jd["Flag"][i]["size"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        //GameObject flag_object = Instantiate(flag,pos,Quaternion.identity);
-        flag = GameObject.Find("Flag");
-        //flag_object.transform.localScale *= size;
-        //Flags.Add(flag_object);
-        flag.transform.position = pos;
-        */
+    //load flag
+    try{
+        for (int i = 0; i < jd["Flag"].Count; i++)
+        {
+            //GameObject flag = GameObject.FindWithTag("Flag");
+            GameObject flag = (GameObject)Resources.Load("Prefabs/GameObject/Flag");
+            float x = float.Parse(jd["Flag"][i]["x"].ToString());
+            float y = float.Parse(jd["Flag"][i]["y"].ToString());
+            float size = float.Parse(jd["Flag"][i]["size"].ToString());
+            float angle = float.Parse(jd["Flag"][i]["angle"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            GameObject flag_object = Instantiate(flag,pos,Quaternion.identity);
+            flag = GameObject.Find("Flag");
+            flag_object.transform.localScale = new Vector3(size,size,1);
+            flag_object.transform.position = pos;
+            flag_object.transform.localEulerAngles = new Vector3(0.0f,0.0f,angle);
+            Flags.Add(flag_object);
+        }
     }
-
+    catch (Exception e) {}
     //load Bound
-    for (int i = 0; i < jd["Bound"].Count; i++)
-    {
-        GameObject bound = (GameObject)Resources.Load("Prefabs/GameObject/Bound");
-        float x = float.Parse(jd["Bound"][i]["x"].ToString());
-        float y = float.Parse(jd["Bound"][i]["y"].ToString());
-        float w = float.Parse(jd["Bound"][i]["w"].ToString());
-        float h = float.Parse(jd["Bound"][i]["h"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        Vector3 shape = new Vector3(w, h, 1f);
-        GameObject bound_object = Instantiate(bound, pos, Quaternion.identity);
-        bound_object.transform.localScale = shape;
-        bound_object.GetComponent<BoxCollider>().isTrigger = false;
-        Bounds.Add(bound_object);
+    try{
+        for (int i = 0; i < jd["Bound"].Count; i++)
+        {
+            print("Bound");
+            GameObject bound = (GameObject)Resources.Load("Prefabs/GameObject/Bound");
+            float x = float.Parse(jd["Bound"][i]["x"].ToString());
+            float y = float.Parse(jd["Bound"][i]["y"].ToString());
+            float w = float.Parse(jd["Bound"][i]["w"].ToString());
+            float h = float.Parse(jd["Bound"][i]["h"].ToString());
+            float angle = float.Parse(jd["Bound"][i]["angle"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            Vector3 shape = new Vector3(w, h, 1f);
+            GameObject bound_object = Instantiate(bound, pos, Quaternion.identity);
+            bound_object.transform.localScale = shape;
+            bound_object.transform.localEulerAngles = new Vector3(0.0f,0.0f,angle);
+            bound_object.GetComponent<BoxCollider>().isTrigger = false;
+            Bounds.Add(bound_object);
+        }
     }
-
+    catch (Exception e) {}
     //load AbsorbBound
-    for (int i = 0; i < jd["AbsorbBound"].Count; i++)
-    {
-        GameObject AbsorbBound = (GameObject)Resources.Load("Prefabs/GameObject/AbsorbBound");
-        float x = float.Parse(jd["AbsorbBound"][i]["x"].ToString());
-        float y = float.Parse(jd["AbsorbBound"][i]["y"].ToString());
-        float w = float.Parse(jd["AbsorbBound"][i]["w"].ToString());
-        float h = float.Parse(jd["AbsorbBound"][i]["h"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        Vector3 shape = new Vector3(w, h, 1f);
-        GameObject AbsorbBound_object = Instantiate(AbsorbBound, pos, Quaternion.identity);
-        AbsorbBound_object.transform.localScale = shape;
-        AbsorbBounds.Add(AbsorbBound_object);
+    try{
+        for (int i = 0; i < jd["AbsorbBound"].Count; i++)
+        {
+            print("AbsorbBound");
+            GameObject AbsorbBound = (GameObject)Resources.Load("Prefabs/GameObject/AbsorbBound");
+            float x = float.Parse(jd["AbsorbBound"][i]["x"].ToString());
+            float y = float.Parse(jd["AbsorbBound"][i]["y"].ToString());
+            float w = float.Parse(jd["AbsorbBound"][i]["w"].ToString());
+            float h = float.Parse(jd["AbsorbBound"][i]["h"].ToString());
+            float angle = float.Parse(jd["AbsorbBound"][i]["angle"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            Vector3 shape = new Vector3(w, h, 1f);
+            GameObject AbsorbBound_object = Instantiate(AbsorbBound, pos, Quaternion.identity);
+            AbsorbBound_object.transform.localScale = shape;
+            AbsorbBound_object.transform.localEulerAngles = new Vector3(0.0f,0.0f,angle);
+            AbsorbBounds.Add(AbsorbBound_object);
+        }
     }
-
+    catch (Exception e) {}
     //load ControlGate
-    for (int i = 0; i < jd["ControlGate"].Count; i++)
-    {
-        GameObject prefab = (GameObject)Resources.Load("Prefabs/GameObject/ControlGate");
-        float x = float.Parse(jd["ControlGate"][i]["x"].ToString());
-        float y = float.Parse(jd["ControlGate"][i]["y"].ToString());
-        float permit_vel = float.Parse(jd["ControlGate"][i]["permit_vel"].ToString());
-        float elastic_ratio = float.Parse(jd["ControlGate"][i]["elastic_ratio"].ToString());
-        float size = float.Parse(jd["ControlGate"][i]["size"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        GameObject control_object = Instantiate(prefab, pos, Quaternion.identity);
-        control_object.transform.localScale *= size;
-        control_object.GetComponent<ControlGate>().permit_vel = permit_vel;
-        control_object.GetComponent<ControlGate>().elastic_ratio = elastic_ratio;
-        ControlGates.Add(control_object);
+    try{
+        for (int i = 0; i < jd["ControlGate"].Count; i++)
+        {
+            print("ControlGate");
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/GameObject/ControlGate");
+            float x = float.Parse(jd["ControlGate"][i]["x"].ToString());
+            float y = float.Parse(jd["ControlGate"][i]["y"].ToString());
+            float permit_vel = float.Parse(jd["ControlGate"][i]["permit_vel"].ToString());
+            float elastic_ratio = float.Parse(jd["ControlGate"][i]["elastic_ratio"].ToString());
+            float size = float.Parse(jd["ControlGate"][i]["size"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            GameObject control_object = Instantiate(prefab, pos, Quaternion.identity);
+            control_object.transform.localScale = new Vector3(size,size,1);
+            control_object.GetComponent<ControlGate>().permit_vel = permit_vel;
+            control_object.GetComponent<ControlGate>().elastic_ratio = elastic_ratio;
+            ControlGates.Add(control_object);
+        }
     }
-
+    catch (Exception e) {}
     //load RotateGate
-    for (int i = 0; i < jd["RotateGate"].Count; i++)
-    {
-        GameObject RotateGate = (GameObject)Resources.Load("Prefabs/GameObject/RotateGate");
-        float x = float.Parse(jd["RotateGate"][i]["x"].ToString());
-        float y = float.Parse(jd["RotateGate"][i]["y"].ToString());
-        float an_vel = float.Parse(jd["RotateGate"][i]["an_vel"].ToString());
-        float angle = float.Parse(jd["RotateGate"][i]["angle"].ToString());
-        float relative_length = float.Parse(jd["RotateGate"][i]["relative_length"].ToString());
-        float size = float.Parse(jd["RotateGate"][i]["size"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        GameObject rotate_gate_object = Instantiate(RotateGate, pos, Quaternion.identity);
-        rotate_gate_object.transform.localScale *= size;
-        rotate_gate_object.GetComponent<RotateGate>().anVel = an_vel;
-        rotate_gate_object.GetComponent<RotateGate>().angle = angle;
-        rotate_gate_object.GetComponent<RotateGate>().relativeLength = relative_length;
-        RotateGates.Add(rotate_gate_object);
+    try{
+        for (int i = 0; i < jd["RotateGate"].Count; i++)
+        {
+            print("RotateGate");
+            GameObject RotateGate = (GameObject)Resources.Load("Prefabs/GameObject/RotateGate");
+            float x = float.Parse(jd["RotateGate"][i]["x"].ToString());
+            float y = float.Parse(jd["RotateGate"][i]["y"].ToString());
+            float an_vel = float.Parse(jd["RotateGate"][i]["an_vel"].ToString());
+            float angle = float.Parse(jd["RotateGate"][i]["angle"].ToString());
+            float relative_length = float.Parse(jd["RotateGate"][i]["relative_length"].ToString());
+            float size = float.Parse(jd["RotateGate"][i]["size"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            GameObject rotate_gate_object = Instantiate(RotateGate, pos, Quaternion.identity);
+            rotate_gate_object.transform.localScale = new Vector3(size,size,1);
+            rotate_gate_object.GetComponent<RotateGate>().anVel = an_vel;
+            rotate_gate_object.GetComponent<RotateGate>().angle = angle;
+            rotate_gate_object.GetComponent<RotateGate>().relativeLength = relative_length;
+            RotateGates.Add(rotate_gate_object);
+        }
     }
+    catch (Exception e) {}
 
     //load Shooter
-    for (int i = 0; i < jd["Shooter"].Count; i++)
-    {
-        GameObject prefab = (GameObject)Resources.Load("Prefabs/GameObject/Shooter");
-        float x = float.Parse(jd["Shooter"][i]["x"].ToString());
-        float y = float.Parse(jd["Shooter"][i]["y"].ToString());
-        float out_vel = float.Parse(jd["Shooter"][i]["out_vel"].ToString());
-        float angle = float.Parse(jd["Shooter"][i]["angle"].ToString());
-        float size = float.Parse(jd["Shooter"][i]["size"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        GameObject Shooter_object = Instantiate(prefab, pos, Quaternion.identity);
-        Shooter_object.transform.localScale *= size;
-        Shooter_object.GetComponent<Shooter>().outVelocity = out_vel;
-        Shooter_object.GetComponent<Shooter>().angle = angle;
-        Shooters.Add(Shooter_object);
+    try{
+        for (int i = 0; i < jd["Shooter"].Count; i++)
+        {
+            print("Shooter");
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/GameObject/Shooter");
+            float x = float.Parse(jd["Shooter"][i]["x"].ToString());
+            float y = float.Parse(jd["Shooter"][i]["y"].ToString());
+            float out_vel = float.Parse(jd["Shooter"][i]["out_vel"].ToString());
+            float angle = float.Parse(jd["Shooter"][i]["angle"].ToString());
+            float size = float.Parse(jd["Shooter"][i]["size"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            GameObject Shooter_object = Instantiate(prefab, pos, Quaternion.identity);
+            Shooter_object.transform.localScale = new Vector3(size,size,1);
+            Shooter_object.GetComponent<Shooter>().outVelocity = out_vel;
+            Shooter_object.GetComponent<Shooter>().angle = angle;
+            Shooters.Add(Shooter_object);
+        }
     }
+    catch (Exception e) {}
 
     //load SpeedGate
-    for (int i = 0; i < jd["SpeedGate"].Count; i++)
-    {
-        GameObject prefab = (GameObject)Resources.Load("Prefabs/GameObject/SpeedGate");
-        float x = float.Parse(jd["SpeedGate"][i]["x"].ToString());
-        float y = float.Parse(jd["SpeedGate"][i]["y"].ToString());
-        float out_vel = float.Parse(jd["SpeedGate"][i]["out_vel"].ToString());
-        float size = float.Parse(jd["SpeedGate"][i]["size"].ToString());
-        Vector3 pos = new Vector3(x, y, 0f);
-        GameObject SpeedGate_object = Instantiate(prefab, pos, Quaternion.identity);
-        SpeedGate_object.transform.localScale *= size;
-        SpeedGate_object.GetComponent<SpeedGate>().vOut = out_vel;
-        SpeedGates.Add(SpeedGate_object);
+    try{
+        for (int i = 0; i < jd["SpeedGate"].Count; i++)
+        {
+            print("SpeedGate");
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/GameObject/SpeedGate");
+            float x = float.Parse(jd["SpeedGate"][i]["x"].ToString());
+            float y = float.Parse(jd["SpeedGate"][i]["y"].ToString());
+            float out_vel = float.Parse(jd["SpeedGate"][i]["out_vel"].ToString());
+            float size = float.Parse(jd["SpeedGate"][i]["size"].ToString());
+            float angle = float.Parse(jd["SpeedGate"][i]["angle"].ToString());
+            Vector3 pos = new Vector3(x, y, 0f);
+            GameObject SpeedGate_object = Instantiate(prefab, pos, Quaternion.identity);
+            SpeedGate_object.transform.localScale = new Vector3(size,size,1);
+            SpeedGate_object.GetComponent<SpeedGate>().vOut = out_vel;
+            SpeedGate_object.transform.localEulerAngles = new Vector3(0.0f,0.0f,angle);
+            SpeedGates.Add(SpeedGate_object);
+        }
     }
-    }
+    catch (Exception e) {}
+
+}
 
     public void resetPlayer()
     {
@@ -315,3 +348,4 @@ public class PlayingManager : MonoBehaviour {
         GameObject Player = Instantiate(player, Player_Defaultpos, Quaternion.identity);
     }
 }
+
